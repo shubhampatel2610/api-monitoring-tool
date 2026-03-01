2933422da049c03bf4baaace9c5652886fce0ab2
shubhampatel2610 <shubhu2610@gmail.com>
2026-03-01 18:30:22 +0530

    commit - global error & loading states

--- app/store/DashboardStore.ts
+++ app/store/DashboardStore.ts
@@ -7,11 +7,29 @@ export class DashboardStore {
         makeAutoObservable(this);
     }
 
+    globalLoading: boolean = false;
+
+    showGlobalError: boolean = false;
+
+    globalErrorMessage: string = "";
+
     searchField = new Field({
         name: AppConstants.SEARCH_FIELD.name,
         label: AppConstants.SEARCH_FIELD.label,
         placeholder: AppConstants.SEARCH_FIELD.placeholder,
     })
+
+    setGlobalLoading = (value: boolean) => {
+        this.globalLoading = value;
+    }
+
+    setGlobalError = (value: boolean) => {
+        this.showGlobalError = value;
+    }
+
+    setGlobalErrorMessage = (value: string) => {
+        this.globalErrorMessage = value;
+    }
 
 }
 
