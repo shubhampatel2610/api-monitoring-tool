b7ca03e9663184b82a7440a68ef0b55a610e1509
shubhampatel2610 <shubhu2610@gmail.com>
2026-03-01 18:29:45 +0530

    commit - layout improvement and global loader

---
+++ app/Providers/ClientLayout.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import { Provider } from "mobx-react";
+import { PrimeReactProvider } from "primereact/api";
+import store from "../store/store";
+import GlobalLoader from "./LoaderProvider/LoaderProvider";
+// import MenubarComponent from "../components/MenubarComponent/MenubarComponent";
+import { Suspense } from "react";
+import dynamic from "next/dynamic";
+const MenubarComponent = dynamic(
+  () => import("../components/MenubarComponent/MenubarComponent"),
+  { ssr: false }
+);
+
+export default function ClientLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <Provider store={store}>
+      <Suspense fallback={<div>Loading...</div>}>
+        <PrimeReactProvider>
+          <GlobalLoader />
+          <MenubarComponent />
+          <div className="common-styling">
+            {children}
+          </div>
+        </PrimeReactProvider>
+      </Suspense>
+    </Provider>
+  );
+}
